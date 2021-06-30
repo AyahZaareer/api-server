35f8fedc67f96f0a5b469cf8bb5a31373d62c5ab
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-07-01 00:53:10 +0300

    not finish

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+require('dotenv').config();
+const server = require('./src/server');
+const mongooes = require('mongoose');
+const PORT = process.env.port || 3000;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+mongooes.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+}).then(() => {
+    server.start(PORT);
+}).catch((e) => {
+    console.error('connection error', e.message);
+
+});
