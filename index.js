b165cbcbde0ab92b9b785d73a7fb24606d759e10
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-07-05 00:47:07 +0300

    done

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const server = require('./src/server');
 const mongooes = require('mongoose');
 const PORT = process.env.port || 3000;
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-mongooes.connect(MONGODB_URL, {
+mongooes.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
