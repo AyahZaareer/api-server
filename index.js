e08e7708717a07ac302ea9d5390b61608d7fd928
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-07-05 00:55:44 +0300

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const server = require('./src/server');
 const mongooes = require('mongoose');
-const PORT = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongooes.connect(MONGODB_URI, {
@@ -10,7 +10,7 @@ mongooes.connect(MONGODB_URI, {
     useUnifiedTopology: true,
     useFindAndModify: false,
 }).then(() => {
-    server.start(PORT);
+    server.start(port);
 }).catch((e) => {
     console.error('connection error', e.message);
 
